0a9a2e5fc47bac3f65b300dbeb24cd0da8940332
DaveRoox <davexrusso@gmail.com>
2018-07-11 00:48:13 +0200

    person -> Person

--- FunctionalExampleC++/src/main.cpp
+++ FunctionalExampleC++/src/main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 using namespace functional;
 
-struct person {
+struct Person {
 
     enum class gender_t { male, female };
 
@@ -21,7 +21,7 @@ struct person {
     int age;
     gender_t gender;
 
-    person(string t_name, string t_last_name, int t_age, gender_t t_gender):
+    Person(string t_name, string t_last_name, int t_age, gender_t t_gender):
             name(std::move(t_name)),
             last_name(std::move(t_last_name)),
             age(t_age),
@@ -31,23 +31,23 @@ struct person {
 
 int main() {
 
-    functional_list<person> people = {
-            person("bob", "postman", 28, person::gender_t::male),
-            person("alice", "postwoman", 27, person::gender_t::female),
-            person("miley", "cyrus", 20, person::gender_t::male),
-            person("john", "smith", 25, person::gender_t::male),
-            person("random", "old boy", 95, person::gender_t::male),
-            person("random", "old girl", 92, person::gender_t::female)
+    functional_list<Person> people = {
+            Person("bob", "postman", 28, Person::gender_t::male),
+            Person("alice", "postwoman", 27, Person::gender_t::female),
+            Person("miley", "cyrus", 20, Person::gender_t::male),
+            Person("john", "smith", 25, Person::gender_t::male),
+            Person("random", "old boy", 95, Person::gender_t::male),
+            Person("random", "old girl", 92, Person::gender_t::female)
     };
 
-    auto print_person = [] (const person & p) { cout << p.name << " " << p.last_name << " is " << p.age << " years old\n"; };
+    auto print_person = [] (const Person & p) { cout << p.name << " " << p.last_name << " is " << p.age << " years old\n"; };
 
     cout << "--First 2 oldest male people are:--\n";
     people
-            .filter([] (const person & p) {
-                return p.gender == person::gender_t::male;
+            .filter([] (const Person & p) {
+                return p.gender == Person::gender_t::male;
             })
-            .sort([] (const person & p1, const person & p2) {
+            .sort([] (const Person & p1, const Person & p2) {
                 return p1.age >= p2.age;
             })
             .limit_to(2)
@@ -57,13 +57,13 @@ int main() {
 
     cout << "--People ages in descending order are:--\n";
     people
-            .map([] (const person & p) {
+            .map([] (const Person & p) {
                 return p.age;
             })
             .sort(true)
             .print() << endl << endl;
 
-    cout << "Last person in the array is:\n";
+    cout << "Last Person in the array is:\n";
     print_person(people[-1]);
 
     cout << endl;
@@ -74,27 +74,27 @@ int main() {
     cout << endl;
 
     cout << "--The average age is:--\n";
-    cout << fixed << setprecision(2) << people.reduce(0.0, [] (double acc, const person & p) -> double { return acc + p.age; }) / people.count() << endl << endl;
+    cout << fixed << setprecision(2) << people.reduce(0.0, [] (double acc, const Person & p) -> double { return acc + p.age; }) / people.count() << endl << endl;
 
     cout << "--The list of uniques names is:--\n";
     people
-            .map([] (const person & p) {
+            .map([] (const Person & p) {
                 return p.name;
             })
             .uniques()
             .print() << endl << endl;
 
     cout << "--Is there at least one girl?--\n";
-    cout << (people.any_match([] (const person & p) { return p.gender == person::gender_t::female; }) ? "YES" : "NO") << endl << endl;
+    cout << (people.any_match([] (const Person & p) { return p.gender == Person::gender_t::female; }) ? "YES" : "NO") << endl << endl;
 
     cout << "--Are there only girls?--\n";
-    cout << (people.each_match([] (const person & p) { return p.gender == person::gender_t::female; }) ? "YES" : "NO") << endl << endl;
+    cout << (people.each_match([] (const Person & p) { return p.gender == Person::gender_t::female; }) ? "YES" : "NO") << endl << endl;
 
     cout << "--The oldest is:--\n";
-    cout << people.max([] (const person & p) { return p.age; }).first().age << " years old" << endl << endl;
+    cout << people.max([] (const Person & p) { return p.age; }).first().age << " years old" << endl << endl;
 
     cout << "--The youngest is:--\n";
-    cout << people.min([] (const person & p) { return p.age; }).first().age << " years old" << endl << endl;
+    cout << people.min([] (const Person & p) { return p.age; }).first().age << " years old" << endl << endl;
 
     return 0;
 }
